3d6d7e559fe3be0cccc05450c7e48e6a0f8f1b7d
Samuel Paul Christopher <thesamchris@gmail.com>
2020-07-31 18:08:05 +0800

    contact form in html for js form

--- app/src/components/pages/contact.js
+++ app/src/components/pages/contact.js
@@ -3,17 +3,17 @@ import React from 'react'
 const ContactPage = () => (
     <div className="contact__content">
         <div className="contact__form">
-            <form action="POST" data-netlify="true">
+            <form action="POST" name="contact_form" data-netlify="true">
                 <label htmlFor="type"></label>
-                <select id="type">
+                <select name="type "id="type">
                     <option>Support</option>
                     <option>Customer Service</option>
                 </select>
-                <input type="text" id="name" placeholder="Name"/>
-                <input type="number" id="contact" placeholder="Contant"/>
-                <input type="email" id="email" placeholder="Email"/>
-                <input type="text" id="subject"/>
-                <input type="text" id="message"/>
+                <input type="text" id="name" name="name" placeholder="Name"/>
+                <input name="contact" type="number" id="contact" placeholder="Contant"/>
+                <input name="email" type="email" id="email" placeholder="Email"/>
+                <input name="subject" type="text" id="subject"/>
+                <input name="message" type="text" id="message"/>
                 <div data-netlify-recaptcha="true"></div>
                 <button type="submit">send message</button>
             </form>
