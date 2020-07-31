98920793c3132273752a883829f82c4da6d27fdc
Samuel Paul Christopher <thesamchris@gmail.com>
2020-07-31 18:27:45 +0800

    retry netlify forms

--- app/src/components/pages/contact.js
+++ app/src/components/pages/contact.js
@@ -3,7 +3,28 @@ import React from 'react'
 const ContactPage = () => (
 	<div className="contact__content">
 		<div className="contact__form">
-			<form action="POST" name="contact_form">
+			<form name="contact" method="post">
+				<input type="hidden" name="form-name" value="contact" />
+				<p>
+					<label>
+						Your Name: <input type="text" name="name" />
+					</label>
+				</p>
+				<p>
+					<label>
+						Your Email: <input type="email" name="email" />
+					</label>
+				</p>
+				<p>
+					<label>
+						Message: <textarea name="message"></textarea>
+					</label>
+				</p>
+				<p>
+					<button type="submit">Send</button>
+				</p>
+			</form>
+			{/* <form action="POST" name="contact_form">
 				<label htmlFor="type"></label>
 				<input type="hidden" name="form-name" value="contact_form" />
 				<select name="type " id="type">
@@ -22,7 +43,7 @@ const ContactPage = () => (
 				<input name="message" type="text" id="message" />
 				<div data-netlify-recaptcha="true"></div>
 				<button type="submit">send message</button>
-			</form>
+			</form> */}
 		</div>
 	</div>
 )
