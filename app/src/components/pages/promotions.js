3175c0bc6d6c7ae7306be23ca37b3c0ccc7c136b
Sam Chris <thesamchris@gmail.com>
2021-05-03 10:10:13 +0800

    updated promotions

--- app/src/components/pages/promotions.js
+++ app/src/components/pages/promotions.js
@@ -7,7 +7,8 @@ const PromotionsPage = () => (
         <Hero title="promotions" subtitle="the best deals" showLink={false} secondaryTitle="best quality at best price" imgClass="promotions_bg"/>
 
         <div className="promotions_page__content">
-            <div className="promotions_page__promo august_promo"></div>
+            <div className="promotions_page__promo yearlya_promo"></div>
+            <div className="promotions_page__promo yearlyb_promo"></div>
         </div>
     </div>
 )
