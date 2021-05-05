cac213e417f9cef5aae9555b45b8567b88cd68bf
Sam Chris <thesamchris@gmail.com>
2021-05-05 13:33:38 +0800

    added wash data

---
+++ app/src/components/pages/products/wash/washTemplate.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const WashTemplate = ({ pricing, exteriorItems, interiorItems }) => (
+    <div>
+        <div>
+            <h1 className="wash_title">Standard Wash</h1>
+        </div>
+        <div className="wash__lists_container">
+            <div className="tick_lists">
+                <p className="tick_lists__title">Exterior:</p>
+                <ul className="fa-ul">
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Exterior Wash with SONAX Foam Energy Shampoo</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Wheels, wheel wells & tires cleaning</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Drying with microfiber towel</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Air blown excess water</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Glass Clean with SONAX Glass Cleaner</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Tires dressing with SONAX Profiline Tire Gloss</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>SONAX High Speed Wax for paint protection</li>
+                </ul>
+            </div>
+            <div className="tick_lists">
+                <p className="tick_lists__title">Interior:</p>
+                <ul className="fa-ul">
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Vacuum</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Interior Wipe</li>
+                    <li><span className="fa-li"><i className="fas fa-check"></i></span>Glass Clean with SONAX Glass Cleaner</li>
+                </ul>
+            </div>
+        </div>
+        <div className="wash_duration">Duration: 30 minutes</div>
+        <div className="wash_pricing__options_container">
+            <div className="wash_pricing__option">
+                <div className="wash_pricing__image"></div>
+                <p className="wash_pricing__size">S/M</p>
+                <p className="wash_pricing__price">{pricing.S}</p>
+            </div>
+            <div className="wash_pricing__option">
+                <div className="wash_pricing__image"></div>
+                <p className="wash_pricing__size">L</p>
+                <p className="wash_pricing__price">{pricing.L}</p>
+            </div>
+            <div className="wash_pricing__option">
+                <div className="wash_pricing__image"></div>
+                <p className="wash_pricing__size">X/L</p>
+                <p className="wash_pricing__price">{pricing.XL}</p>
+            </div>
+        </div>
+        <hr />
+        <div className="wash_vehicle__reference"></div>
+    </div>
+)
+
+export default WashTemplate
---
+++ app/src/data/washDetails.js
@@ -0,0 +1,90 @@
+export default {
+    standardWash: {
+        duration: '30 minutes',
+        S: '$15',
+        L: '$$20',
+        XL: '$25 – $30',
+        exteriorItems: [
+            'Exterior Wash with SONAX Foam Energy Shampoo',
+            'Wheels, wheel wells & tires cleaning',
+            'Drying with microfiber towel',
+            'Air blown excess water',
+            'Glass Clean with SONAX Glass Cleaner',
+            'Tires dressing with SONAX Profiline Tire Gloss',
+            'SONAX High Speed Wax for paint protection'
+        ],
+        interiorItems: [
+            'Vacuum',
+            'Interior Wipe',
+           ' Glass Clean with SONAX Glass Cleaner'
+        ]
+    },
+    deluxeWash: {
+        duration: '40 minutes',
+        S: '$25',
+        L: '$$35',
+        XL: '$45 – $50',
+        exteriorItems: [
+            'Exterior Wash with SONAX Foam Energy Shampoo',
+            'Wheels, wheel wells & tires cleaning',
+            'Drying with microfiber towel',
+            'Air blown excess water',
+            'Glass Clean with SONAX Glass Cleaner',
+            'Tires dressing with SONAX Profiline Tire Gloss',
+            'SONAX High Speed Wax for paint protection',
+            'SONAX Brilliant Shine Detailer for boosting protection on paint'
+        ],
+        interiorItems: [
+            'Vacuum',
+            'Interior Wipe',
+           ' Glass Clean with SONAX Glass Cleaner'
+        ]
+    },
+    premiumWash: {
+        duration: '45 minutes',
+        S: '$35',
+        L: '$$30',
+        XL: '$65 – $70',
+        exteriorItems: [
+            'Exterior Wash with SONAX Foam Energy Shampoo',
+            'Wheels, wheel wells & tires cleaning',
+            'Drying with microfiber towel',
+            'Air blown excess water',
+            'Glass Clean with SONAX Glass Cleaner',
+            'Tires dressing with SONAX Profiline Tire Gloss',
+            'SONAX High Speed Wax for paint protection',
+            'SONAX Brilliant Shine Detailer for boosting protection on paint'
+        ],
+        interiorItems: [
+            'Vacuum',
+            'Trim Wipe with SONAX Multistar (All-Purpose Cleaner)',
+            'Trim Dressing with SONAX Plastic Care (UV protection)',
+            'Glass Clean with SONAX Glass Cleaner'
+        ]
+    },
+    eliteWash: {
+        duration: '60 minutes',
+        S: '$45',
+        L: '$$65',
+        XL: '$85 – $90',
+        exteriorItems: [
+            'Exterior Wash with SONAX Foam Energy Shampoo',
+            'SONAX Wheel Cleaner Full Effect to remove stubborn brake dust on rims',
+            'Wheels, wheel wells & tires cleaning',
+            'Brushing all doors jambs, trunk jambs, exterior narrow opening & crevices with SONAX Multistar (All-Purpose Cleaner)',
+            'SONAX Spray & Seal for paint protection',
+            'Drying with microfiber towel',
+            'Air blown excess water',
+            'Glass Clean with SONAX Glass Cleaner',
+            'Tires dressing with SONAX Profiline Tire Gloss',
+            'SONAX High Speed Wax for paint protection',
+            'SONAX Brilliant Shine Detailer for boosting protection on paint'
+        ],
+        interiorItems: [
+            'Vacuum',
+            'Trim Wipe with SONAX Multistar (All-Purpose Cleaner)',
+            'Trim Dressing with SONAX Plastic Care (UV protection)',
+            'Glass Clean with SONAX Glass Cleaner'
+        ]
+    },
+}
